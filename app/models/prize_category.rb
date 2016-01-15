42e1e735168433c502d1a80befe11c756d8881ac
Qaisar Nadeem <iamqaisarnadeen@gmail.com>
2016-01-15 13:14:52 +0500

    time zone set

--- app/models/prize_category.rb
+++ app/models/prize_category.rb
@@ -2,7 +2,7 @@ class PrizeCategory < ActiveRecord::Base
   validates_presence_of :title,:amount
   validates :amount ,:numericality=>{:greater_than=>0}
   has_many :country_specific_prizes
-
+  has_many :prize_redemptions
   has_attached_file :icon, styles: { medium: "300x300>"}, default_url: "/assets/:style/missing.png"
   validates_attachment_content_type :icon, content_type: /\Aimage\/.*\Z/
  
